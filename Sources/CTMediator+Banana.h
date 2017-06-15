a8ada7db012bcdd8a5ade89c6351d91372f5bc10
Stephen Chiang <jianghaoyuan2007@gmail.com>
2017-06-15 17:03:51 +0800

    Added basic features to BananaConnector.

---
+++ Sources/CTMediator+Banana.h
@@ -0,0 +1,16 @@
+//
+//  CTMediator+Banana.h
+//  BananaConnector
+//
+//  Created by Stephen Chiang on 15/06/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <CTMediator/CTMediator.h>
+
+@interface CTMediator(Banana)
+
+- (UIViewController *)Banana_BananaViewController;
+
+@end
